d3a14dc2f579875ceb31d91dad06d103ed99293f
syntax-punk <david.jaeren@gmail.com>
2024-11-22 12:43:09 +0100

    Basic gaming logic is done

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
@@ -9,8 +10,47 @@ public class Quiz : MonoBehaviour
     [SerializeField]
     QuestionSO Challenge;
 
+    [SerializeField]
+    GameObject[] AnswerButtons;
+
+    [SerializeField]
+    Sprite DefaultButtonSprite;
+
+    [SerializeField]
+    Sprite SuccessButtonSprite;
+
     void Start()
     {
         ChallengeText.text = Challenge.GetChallenge();
+
+        for (int i = 0; i < AnswerButtons.Length; i++)
+        {
+            var buttonText = AnswerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = Challenge.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        var correctAnswerIndex = Challenge.GetCorrectAnswerIndex();
+
+        if (index == correctAnswerIndex)
+        {
+            ChallengeText.text = "Correct!";
+            buttonImage = AnswerButtons[index]
+                .GetComponent<Image>();
+
+            buttonImage.sprite = SuccessButtonSprite;
+        }
+        else
+        {
+            var correctAnswer = Challenge.GetAnswer(correctAnswerIndex);
+            ChallengeText.text = "Opps! Correct answer is: \n" + correctAnswer;
+
+            buttonImage = AnswerButtons[correctAnswerIndex]
+                .GetComponent<Image>();
+            buttonImage.sprite = SuccessButtonSprite;
+        }
     }
 }
